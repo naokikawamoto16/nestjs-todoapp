9ac0f11121423a6d44ee66bca4bf5b1680a56eff
Naoki Kawamoto <naokikawamoto16@gmail.com>
2025-01-03 22:35:23 +0900

    feat: add task and user management modules with basic CRUD functionality

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { Task } from './interface/task.interface';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [
+    { id: 1, name: 'Task 1', completed: true },
+    { id: 2, name: 'Task 2', completed: false }
+  ]
+
+  create(createTaskDto: CreateTaskDto) {
+    const id = this.tasks.length + 1
+    const task: Task = {
+      id,
+      name: createTaskDto.name,
+      completed: false
+    }
+    this.tasks.push(task)
+    return task
+  }
+}
