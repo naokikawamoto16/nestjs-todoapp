94c52a8bea648709f61b93b90b8c84713214119c
Naoki Kawamoto <naokikawamoto16@gmail.com>
2025-01-26 14:17:35 +0900

    feat: remove dueDate and parentTaskId columns from Task model and update migration

---
+++ backend/prisma/migrations/20250126050539_deleted_parent_task/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `dueDate` on the `Task` table. All the data in the column will be lost.
+  - You are about to drop the column `parentTaskId` on the `Task` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Task" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    "userId" INTEGER NOT NULL,
+    CONSTRAINT "Task_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Task" ("completed", "createdAt", "id", "name", "updatedAt", "userId") SELECT "completed", "createdAt", "id", "name", "updatedAt", "userId" FROM "Task";
+DROP TABLE "Task";
+ALTER TABLE "new_Task" RENAME TO "Task";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;
